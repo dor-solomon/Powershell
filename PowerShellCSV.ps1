90277ff8634f0ef0c95f7fa0deaba916d5934ac2
Dor Solomon <68469554+dor-solomon@users.noreply.github.com>
2022-04-05 13:08:13 +0000

    Add CSV column to AD script

---
+++ PowerShellCSV.ps1
@@ -0,0 +1,16 @@
+﻿# PowerShell Scripts
+# Copyright © 2022 Dor Solomon
+# SPDX-License-Identifier: AGPL-3.0-only
+
+$csvfile = Read-Host "Enter the csv file's path ($home\Desktop\list.csv)"
+$column = Read-Host "Enter the column's name"
+$ADattribute = Read-Host "Enter the Active Directory attribute"
+$csv = Import-Csv $csvfile
+$headers = $csv | Get-Member -MemberType NoteProperty | select -Expand Name
+$output =
+   foreach ($data in $csv.$column) {
+        $activedata = $data.Substring(0,$data.Length-1)
+        Get-ADUser -Filter '$ADattribute -like $activedata' -Properties $ADattribute | Select-Object SamAccountName
+      }
+$ofile = Read-Host "Enter the output file ($home\Desktop\output.txt)"
+$output | Out-File $ofile
