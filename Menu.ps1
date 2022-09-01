a79d66066dc2501d2e4d9f3419cd4074549dd212
Dor Solomon <68469554+dor-solomon@users.noreply.github.com>
2022-09-01 15:31:32 +0300

    Added 2 options

--- Menu.ps1
+++ Menu.ps1
@@ -1,36 +1,48 @@
-# PowerShell Scripts
-# Copyright © 2022 Dor Solomon
-# SPDX-License-Identifier: GPL-3.0-only
-    
+# Powershell Scripts
+# Copyright � 2022 Dor Solomon
+# SPDX-License-Identifier: AGPL-3.0-only
+
 $Test = Read-Host "Type the AD group you require"
 function Show-Menu
 {
      param (
            [string]$Title = 'My Menu'
      )
-     Clear-Host
+     cls
      Write-Host "================ $Title ================"
     
-     Write-Host "1: Press '1' for full names in a text file."
-     Write-Host "2: Press '2' for usernames in a csv file."
+     Write-Host "1: Press '1' for a list of full names in a text file."
+     Write-Host "2: Press '2' for a list of usernames in a csv file."
+     Write-Host "3: Press '3' for a list of full names in a csv file."
+	 Write-Host "2: Press '4' for a list of usernames in a text file."
      Write-Host "Q: Press 'Q' to quit."
 }
 
 do
 {
      Show-Menu
-     $selection = Read-Host "Please make a selection"
-     switch ($selection)
+     $input = Read-Host "Please make a selection"
+     switch ($input)
      {
            '1' {
-                Clear-Host
-                'You chose option #1 for full names in a text file.'
-                Get-ADGroupMember -Identity "$Test" | Select-Object @{Name="ùí îìà";Expression={$_.Name}} | Sort-Object "ùí îìà" | Out-File $home\Desktop\Names.txt
+                cls
+                'You chose option #1 for a list of full names in a text file.'
+                Get-ADGroupMember -Identity "$Test" | Select-Object @{Name="�� ���";Expression={$_.Name}} | Sort-Object "�� ���" | Out-File $home\Desktop\Names.txt
                 'Completed.'
            } '2' {
-                Clear-Host
-                'You chose option #2 for usernames in a csv file.'
+                cls
+                'You chose option #2 for a list of usernames in a csv file.'
                 Get-ADGroupMember -Identity "$Test" | Select-Object @{Name="User Name";Expression={$_.SamAccountName}} | Sort-Object "User Name" | Export-CSV -NoTypeInformation -Path $home\Desktop\Names.csv -Encoding UTF8
+                'Completed.'
+           } '3' {
+                cls
+                'You chose option #3 for a list of full names in a csv file.'
+                Get-ADGroupMember -Identity "$Test" | Select-Object @{Name="�� ���";Expression={$_.Name}} | Sort-Object "�� ���" | Export-CSV -NoTypeInformation -Path $home\Desktop\Names.csv -Encoding UTF8
+                'Completed.'
+           } '4' {
+                cls
+                'You chose option #4 for a list of usernames in a text file.'
+                Get-ADGroupMember -Identity "$Test" | Select-Object @{Name="User Name";Expression={$_.SamAccountName}} | Sort-Object "User Name" | Out-File $home\Desktop\Names.txt
                 'Completed.'
            } 'q' {
                 return
